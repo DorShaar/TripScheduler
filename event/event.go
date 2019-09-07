2187f4a160ba1d235aa6af9c9fc6c8584a732143
Dor Shaar <dorsr@mta.ac.il>
2019-09-07 15:45:14 +0300

    Add EndingTime, StartingTime, AreCoinside methods

--- event/event.go
+++ event/event.go
@@ -6,19 +6,48 @@ import (
 )
 
 type Event struct {
-	EventName      string
-	StartingTime   time.Time
-	Duration       time.Duration
-	PrecautionTime time.Duration
-	Location       string
+	EventName string
+	Location  string
+	EventTime EventTime
 }
 
-func (e Event) PrintEvent() {
-	fmt.Printf("'%s', located on %s, starts on %s %s for %.1f hours. Should Come %.0f minutes before.",
+func (e Event) PrintEvent() string {
+	eventInfo := fmt.Sprintf("'%s', located on %s, starts on %s %s for %.1f hours. Should Come %.0f minutes before.",
 		e.EventName,
 		e.Location,
-		e.StartingTime.Weekday(),
-		e.StartingTime.Format("2006-01-02 15:04"),
-		e.Duration.Hours(),
-		e.PrecautionTime.Minutes())
+		e.EventTime.ActualStartingTime.Weekday(),
+		e.EventTime.ActualStartingTime.Format("2006-01-02 15:04"),
+		e.EventTime.Duration.Hours(),
+		e.EventTime.PrecautionTime.Minutes())
+
+	return eventInfo
+}
+
+type EventTime struct {
+	ActualStartingTime time.Time
+	Duration           time.Duration
+	PrecautionTime     time.Duration
+}
+
+// The ActualStartingTime minus the PrecautionTime
+func (et EventTime) StartingTime() time.Time {
+	return et.ActualStartingTime.Add(-et.PrecautionTime)
+}
+
+// The ActualStartingTime plus the Duration of event
+func (et EventTime) EndingTime() time.Time {
+	return et.ActualStartingTime.Add(et.Duration)
+}
+
+// The ActualStartingTime minus the PrecautionTime
+func (currentEventTime EventTime) AreCoincide(eventTime EventTime) bool {
+	if eventTime.StartingTime().After(currentEventTime.EndingTime()) {
+		return false
+	}
+
+	if eventTime.EndingTime().Before(currentEventTime.StartingTime()) {
+		return false
+	}
+
+	return true
 }

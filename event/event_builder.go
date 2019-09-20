8e196eeb2dfbc6d9126c32c99d602e0950d813ee
Dor Shaar <dorsr@mta.ac.il>
2019-09-20 18:17:18 +0300

    Create event builder from YAML files + UT

---
+++ event/event_builder.go
@@ -0,0 +1,111 @@
+package event
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"time"
+	"unicode"
+
+	"gopkg.in/yaml.v2"
+)
+
+var daysOfWeek = map[string]time.Weekday{}
+
+func init() {
+	for d := time.Sunday; d <= time.Saturday; d++ {
+		name := d.String()
+		daysOfWeek[name] = d
+		daysOfWeek[name[:3]] = d
+	}
+}
+
+type eventInterface struct {
+	Name                string
+	Location            string
+	Time_Range          []string
+	Duration            string
+	Precaution_Duration string
+}
+
+func BuildEventFronYAML(yamlPath string) (Event, error) {
+	var eventResult Event
+	// Reads yaml file
+	yamlData, err := ioutil.ReadFile(yamlPath)
+	if err != nil {
+		fmt.Printf("Error reading yaml file. Error:   #%v ", err)
+		return eventResult, err
+	}
+
+	eventData := eventInterface{}
+	err = yaml.Unmarshal(yamlData, &eventData)
+	if err != nil {
+		fmt.Printf("Error unmarsheling yaml data. Error:   #%v ", err)
+		return eventResult, err
+	}
+
+	duration, _ := time.ParseDuration(eventData.Duration)
+	precautionDuration, _ := time.ParseDuration(eventData.Precaution_Duration)
+
+	eventResult.EventName = eventData.Name
+	eventResult.Location = eventData.Location
+	eventResult.TimeRangesList = parseTimeRanges(eventData.Time_Range)
+	eventResult.EventTime.Duration = duration
+	eventResult.EventTime.PrecautionDuration = precautionDuration
+
+	return eventResult, err
+}
+
+func parseTimeRanges(timeRangesStrings []string) []TimeRange {
+	timeRanges := make([]TimeRange, 0)
+
+	for _, currentTimeRange := range timeRangesStrings {
+		var rangeTimeOnly string
+		var day string
+
+		index := strings.IndexByte(currentTimeRange, ' ')
+
+		if index == -1 {
+			// Must be the case of 07:00
+			rangeTimeOnly = currentTimeRange
+		} else if unicode.IsDigit(rune(currentTimeRange[index-1])) {
+			// In case of format 07:00 - 19:00
+			rangeTimeOnly = currentTimeRange
+		} else {
+			// In case of format Sunday: 07:00 - 19:00
+			day = currentTimeRange[:index]
+			rangeTimeOnly = currentTimeRange[index:]
+		}
+
+		timeRange := parseTimeRange(rangeTimeOnly)
+		timeRange.weekday = day
+		timeRanges = append(timeRanges, timeRange)
+	}
+
+	return timeRanges
+}
+
+// Get string in the format of "08:00 - 19:00" and returns starting time as 08:00 and ending
+// time as 19:00.
+// In case there is no ending time such as 08:00, only starting time will be updated.
+func parseTimeRange(timeRangesStr string) TimeRange {
+	timeLayout := "15:04"
+	var endingTime time.Time
+
+	timeRangeSlice := strings.Split(timeRangesStr, "-")
+
+	startingTime, _ := time.Parse(timeLayout, strings.TrimSpace(timeRangeSlice[0]))
+	if len(timeRangeSlice) > 1 {
+		endingTime, _ = time.Parse(timeLayout, strings.TrimSpace(timeRangeSlice[1]))
+	}
+
+	return CreateRange(startingTime, endingTime)
+}
+
+func parseWeekday(dayStr string) (time.Weekday, error) {
+	if day, ok := daysOfWeek[dayStr]; ok {
+		return day, nil
+	}
+
+	panic("invalid weekday")
+}
---
+++ event/event_builder_test.go
@@ -0,0 +1,55 @@
+package event
+
+import (
+	"testing"
+	"time"
+)
+
+func Test_BuildEventFronYAML_TimeRangeWithOneValue(t *testing.T) {
+	expectedDuration, _ := time.ParseDuration("2h")
+	expectedPrecautionDuration, _ := time.ParseDuration("15m")
+	expectedStartingTime, _ := time.Parse("22:22", "07:00")
+	expectedEndingTime, _ := time.Parse("22:22", "19:00")
+
+	event, _ := BuildEventFronYAML("test_files/hyde_park.txt")
+	if event.EventName != "Walking in Hyde Park" ||
+		event.Location != "Hyde Park" ||
+		event.EventTime.Duration != expectedDuration ||
+		event.EventTime.PrecautionDuration != expectedPrecautionDuration ||
+		event.TimeRangesList[0].StartingTime() == expectedStartingTime ||
+		event.TimeRangesList[0].EndingTime() == expectedEndingTime {
+		t.Errorf("Event time could not be parsed from YAML file well")
+	}
+}
+
+func Test_BuildEventFronYAML_TimeRangeWithTwoValues(t *testing.T) {
+	timeLayout := "15:04"
+
+	expectedDay1 := "Sunday"
+	expectedStartingTime1, _ := time.Parse(timeLayout, "07:00")
+	expectedEndingTime1, _ := time.Parse(timeLayout, "15:00")
+
+	expectedDay2 := "Saturday"
+	expectedStartingTime2, _ := time.Parse(timeLayout, "08:00")
+	expectedEndingTime2, _ := time.Parse(timeLayout, "22:00")
+
+	event, _ := BuildEventFronYAML("test_files/camden_town.txt")
+	if event.TimeRangesList[0].weekday != expectedDay1 ||
+		event.TimeRangesList[0].StartingTime() != expectedStartingTime1 ||
+		event.TimeRangesList[0].EndingTime() != expectedEndingTime1 ||
+		event.TimeRangesList[1].weekday != expectedDay2 ||
+		event.TimeRangesList[1].StartingTime() != expectedStartingTime2 ||
+		event.TimeRangesList[1].EndingTime() != expectedEndingTime2 {
+		t.Errorf("Event time could not be parsed from YAML file well")
+	}
+}
+
+func Test_BuildEventFronYAML_TimeRangeWithStartingTimeOnly(t *testing.T) {
+	timeLayout := "15:04"
+	expectedStartingTime, _ := time.Parse(timeLayout, "19:30")
+
+	event, _ := BuildEventFronYAML("test_files/lion_king.txt")
+	if event.TimeRangesList[0].StartingTime() != expectedStartingTime {
+		t.Errorf("Event time could not be parsed from YAML file well")
+	}
+}

16aa2787638ca8c5e45e7146000a4681f501c2cb
Dor Shaar <dorsr@mta.ac.il>
2019-09-22 00:05:16 +0300

    Adapt UT to code changes (AddEvent to TryAddEvent)

--- schedule/schedule_test.go
+++ schedule/schedule_test.go
@@ -8,7 +8,7 @@ import (
 
 const timeFormat = "Jan _2, 2006 15:04"
 
-func Test_AddEvent_EmptySchedule_Success(t *testing.T) {
+func Test_TryAddEvent_EmptySchedule_Success(t *testing.T) {
 	actualStartingTime, _ := time.Parse(timeFormat, "Sep 13, 2019 04:00")
 	duration, _ := time.ParseDuration("90m")
 	precautionDuration, _ := time.ParseDuration("25m")
@@ -16,13 +16,13 @@ func Test_AddEvent_EmptySchedule_Success(t *testing.T) {
 	event := CreateEvent(actualStartingTime, duration, precautionDuration)
 
 	schedule := Schedule{}
-	schedule.AddEvent(event)
-	if len(schedule.EventsList) != 1 {
+	schedule.TryAddEvent(event)
+	if len(schedule.eventsList) != 1 {
 		t.Error("Event was not added to the schedule")
 	}
 }
 
-func Test_AddEvent_Success(t *testing.T) {
+func Test_TryAddEvent_Success(t *testing.T) {
 	// Setting first event.
 	actualStartingTime1, _ := time.Parse(timeFormat, "Sep 13, 2019 04:00")
 	duration1, _ := time.ParseDuration("90m")
@@ -31,7 +31,7 @@ func Test_AddEvent_Success(t *testing.T) {
 	event1 := CreateEvent(actualStartingTime1, duration1, precautionDuration1)
 
 	schedule := Schedule{}
-	schedule.AddEvent(event1)
+	schedule.TryAddEvent(event1)
 
 	// Setting second event.
 	actualStartingTime2, _ := time.Parse(timeFormat, "Sep 13, 2019 08:00")
@@ -39,14 +39,14 @@ func Test_AddEvent_Success(t *testing.T) {
 	precautionDuration2, _ := time.ParseDuration("25m")
 
 	event2 := CreateEvent(actualStartingTime2, duration2, precautionDuration2)
-	schedule.AddEvent(event2)
+	schedule.TryAddEvent(event2)
 
-	if len(schedule.EventsList) != 2 {
+	if len(schedule.eventsList) != 2 {
 		t.Error("Event was not added to the schedule")
 	}
 }
 
-func Test_AddEvent_Fail(t *testing.T) {
+func Test_TryAddEvent_Fail(t *testing.T) {
 	// Setting first event.
 	actualStartingTime1, _ := time.Parse(timeFormat, "Sep 13, 2019 04:00")
 	duration1, _ := time.ParseDuration("90m")
@@ -55,7 +55,7 @@ func Test_AddEvent_Fail(t *testing.T) {
 	event1 := CreateEvent(actualStartingTime1, duration1, precautionDuration1)
 
 	schedule := Schedule{}
-	schedule.AddEvent(event1)
+	schedule.TryAddEvent(event1)
 
 	// Setting second event.
 	actualStartingTime2, _ := time.Parse(timeFormat, "Sep 13, 2019 05:00")
@@ -63,9 +63,9 @@ func Test_AddEvent_Fail(t *testing.T) {
 	precautionDuration2, _ := time.ParseDuration("25m")
 
 	event2 := CreateEvent(actualStartingTime2, duration2, precautionDuration2)
-	schedule.AddEvent(event2)
+	schedule.TryAddEvent(event2)
 
-	if len(schedule.EventsList) != 1 {
+	if len(schedule.eventsList) != 1 {
 		t.Error("Schedule should have only one event")
 	}
 }

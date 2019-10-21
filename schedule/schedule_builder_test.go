b9b4eb2dfbdfd29161e3802bfa65c5d52c92a13a
Dor Shaar <dorsr@mta.ac.il>
2019-10-21 19:03:17 +0300

    Delete scheduler_builder_test

--- schedule/schedule_builder_test.go
+++
@@ -1,7 +0,0 @@
-package schedule
-
-import "testing"
-
-func Test_BuildSchedulesFromFiles(t *testing.T) {
-	BuildSchedulesFromFiles("C:\\Users\\Public\\DorShaar\\GolangWork\\src\\trip_scheduler\\db\\files\\stav_third_year")
-}
